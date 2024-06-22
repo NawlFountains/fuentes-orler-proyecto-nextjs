a7567a0c2af051bc811913cad2c4aed2bc8ddde5
Emanuel Orler <emanuel.orler@gmail.com>
2024-06-21 22:41:14 -0300

    adding route

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,13 @@ import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import Navbar from './ui/dashboard/navbar';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased mx-5`}>
+      <body className={`${inter.className} antialiased bg-dark-gray text-white mx-5`}>
         <Navbar />
         {children}
-        </body>
+      </body>
     </html>
   );
 }
